a316d1dc40f45ee48f7fd2a04e500030a1801888
kisautoisa <v.3916.saki0404@gmail.com>
2024-10-07 12:29:45 +0000

    [Add] Comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,7 +1,19 @@
 class Public::CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post_id)
+    else
+      render 'post/show'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
